df6537deeffe37ca3db349a28380569dc0631423
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-12 11:40:33 +0100

    created post route to test adding new recipe

--- app.js
+++ app.js
@@ -21,11 +21,23 @@ app.get("/api/recipes", async function (req, res) {
   console.log("Get recipes. Test");
 });
 
+// get recipe by id
 app.get("/api/recipes/:id", async function (req, res) {
   const recipe = await getRecipeByID(req.params.id);
   res.send(recipe);
   console.log(`Testing for id: ${req.params.id}`);
 });
+
+// add recipe
+app.post("/api/recipes", async function (req, res) {
+  const newRecipe = await createRecipe(req.body);
+  res.send(newRecipe);
+  console.log(`Testing for new recipe`);
+});
+
+// update recipe
+
+// delete recipe
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
