a2fd2e1f3c635493debc954bc36908d91c08b074
Christopher Webb <webbophone@gmail.com>
2023-05-12 15:40:00 +0100

    Buttons should now work.

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 // Define the URL for the API
-const url = "http://localhost:3000";
+const url = "http://localhost:4000";
 // Select DOM elements
 const recipesSection = document.querySelector("#recipes");
 const getRecipeButton = document.querySelector("#get-recipes");
@@ -29,6 +29,7 @@ function handleSubmit(event) {
   event.preventDefault();
   // Send POST request to API to create new recipe
   createRecipe();
+  // getRecipes();
 }
 // Async function to create new recipe by sending POST request to API
 async function createRecipe() {
@@ -66,37 +67,53 @@ async function getRecipes() {
   const { payload } = await response.json();
   recipesSection.innerHTML = "";
   console.log(payload);
-  payload.forEach(renderRecipe);
+
+  // insert each payload item (recipe) in the recipes section
+  payload.forEach((item) =>
+    recipesSection.insertAdjacentHTML(
+      "beforeend",
+      `<div className="recipe">
+      <h2>${item.title}</h2>
+      <img src=${item.image} alt="recipe-image" className="recipe-image" />
+<h3>Ingredients:</h3>
+  <p>${item.ingredients}</p>
+  <h3>Instructions:</h3>
+<p>${item.instructions}</p>
+      </div>`
+    )
+  );
+
+  // payload.forEach(renderRecipe);
 }
 // Function to render recipe on the page
-function renderRecipe(recipe) {
-  const article = createRecipeView(recipe);
-  recipesSection.appendChild(article);
-}
+// function renderRecipe(recipe) {
+//   const article = createRecipeView(recipe);
+//   recipesSection.appendChild(article);
+// }
 // Function to create HTML element for a recipe
-function createRecipeView({ title, ingredients, instructions, image }) {
-  const article = document.createElement("article");
-  const h2 = document.createElement("h2");
-  h2.innerText = title;
-  const p = document.createElement("p");
-  p.innerText = instructions;
-  const img = document.createElement("img");
-  img.src = image;
-  img.alt = title;
-  const list = createIngredientsList(ingredients);
-  article.appendChild(h2);
-  article.appendChild(img);
-  article.appendChild(list);
-  article.appendChild(p);
-  return article;
-}
+// function createRecipeView({ title, ingredients, instructions, image }) {
+//   const article = document.createElement("article");
+//   const h2 = document.createElement("h2");
+//   h2.innerText = title;
+//   const p = document.createElement("p");
+//   p.innerText = instructions;
+//   const img = document.createElement("img");
+//   img.src = image;
+//   img.alt = title;
+//   const list = createIngredientsList(ingredients);
+//   article.appendChild(h2);
+//   article.appendChild(img);
+//   article.appendChild(list);
+//   article.appendChild(p);
+//   return article;
+// }
 // Function to create HTML element for a list of ingredients
-function createIngredientsList(ingredients) {
-  const ul = document.createElement("ul");
-  ingredients.map(createIngredient).forEach(function (item) {
-    ul.appendChild(item);
-  });
-  return ul;
-}
+// function createIngredientsList(ingredients) {
+//   const ul = document.createElement("ul");
+//   ingredients.map(createIngredient).forEach(function (item) {
+//     ul.appendChild(item);
+//   });
+//   return ul;
+// }
 // Call getRecipes function to display recipes on page when page is loaded
 getRecipes();
